a9b03090ac34135a272e62304165daf1587e6acc
JiHo <gkdl7370@gmail.com>
2026-04-09 10:50:21 +0900

    refactor: inject gateway port from properties

--- src/main/java/com/massflux/gateway/GatewayApplication.java
+++ src/main/java/com/massflux/gateway/GatewayApplication.java
@@ -1,6 +1,7 @@
 package com.massflux.gateway;
 
 import com.massflux.gateway.core.NettyServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +15,9 @@ public class GatewayApplication {
 	}
 
 	@Bean
-	public CommandLineRunner runner(NettyServer nettyServer) {
-		return args -> {
-			// Netty 서버를 8003 포트로 시작
-			nettyServer.start(8003);
-		};
+	public CommandLineRunner runner(
+			NettyServer nettyServer,
+			@Value("${gateway.port}") int port) {
+		return args -> nettyServer.start(port);
 	}
 }
